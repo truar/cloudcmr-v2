d2857cb59197ff48f3bd9cfff4ed61929c0db208
truar <thibault.ruaro@gmail.com>
2019-12-14 11:47:40 +0100

    Add Spring boot 2.2.2 to parent

---
+++ cloudcmr-back/src/main/java/cloud/cmr/com/cloudcmr/CloudcmrApplication.java
@@ -0,0 +1,13 @@
+package cloud.cmr.com.cloudcmr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudcmrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudcmrApplication.class, args);
+	}
+
+}
---
+++ cloudcmr-back/src/test/java/cloud/cmr/com/cloudcmr/CloudcmrApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.cmr.com.cloudcmr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudcmrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
