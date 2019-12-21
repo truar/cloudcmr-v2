81baf806d55d0e3977202e37fc20f732369a1e66
truar <thibault.ruaro@gmail.com>
2019-12-21 16:31:30 +0100

    Fix build when I changed esLint file without knowing what I  was doing

--- cloudcmr-front/.eslintrc.js
+++ cloudcmr-front/.eslintrc.js
@@ -1,18 +1,36 @@
 module.exports = {
   root: true,
   env: {
-    es6: true,
-    node: true,
-    jest: true
+    node: true
   },
-  extends: [
+  'extends': [
     'plugin:vue/essential',
     '@vue/standard'
   ],
-  parser: 'babel-eslint',
-  plugins: ['jest'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)'
+      ],
+      env: {
+        mocha: true
+      }
+    },
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
 }
