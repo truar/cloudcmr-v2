274eb145d67710b08238165097d7a14dd0392264
truar <thibault.ruaro@gmail.com>
2019-12-23 19:34:59 +0100

    Test unit fixed

--- cloudcmr-front/tests/e2e/step_definitions/test.step.js
+++ cloudcmr-front/tests/e2e/step_definitions/test.step.js
@@ -1,10 +1,10 @@
-import { when, then } from 'cypress-cucumber-preprocessor/steps';
-import HomePage from './pages/home.page';
+import { when, then } from 'cypress-cucumber-preprocessor/steps'
+import HomePage from './pages/home.page'
 
 when(/^I open the Home page$/, () => {
-    HomePage.goTo();
-});
+  HomePage.goTo()
+})
 
 then(/^I see "([^"]*)" in the main heading$/, msg => {
-    cy.contains('h1', msg)
-});
+  cy.contains('h1', msg)
+})
