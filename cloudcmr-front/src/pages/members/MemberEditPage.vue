c866fb47e718ce7aefae341279935d321f83b10f
truar <thibault.ruaro@gmail.com>
2020-09-07 12:42:53 +0200

    Try to split into sub-components

--- cloudcmr-front/src/pages/members/MemberEditPage.vue
+++ cloudcmr-front/src/pages/members/MemberEditPage.vue
@@ -3,98 +3,9 @@
         <template slot="subtitle">Adhérents</template>
         <v-row>
             <v-col md="4">
-                <v-card :loading="loading">
-                    <v-form>
-                        <v-card-title>
-                            <span class="text-h6"><span class="font-weight-medium">{{
-                                    memberFullName
-                                }}</span></span>
-                        </v-card-title>
-                        <v-card-text>
-                            <v-container>
-                                <v-row>
-                                    <v-col cols="12">
-                                        <v-text-field
-                                            dense
-                                            label="Nom *"
-                                            v-model="member.lastName"
-                                            type="text"
-                                            :error-messages="lastNameErrors"
-                                            @input="$v.member.lastName.$touch()"
-                                            @blur="$v.member.lastName.$touch()"
-                                            required
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col cols="12">
-                                        <v-text-field
-                                            dense
-                                            label="Prénom *"
-                                            v-model="member.firstName"
-                                            type="text"
-                                            :error-messages="firstNameErrors"
-                                            @input="$v.member.firstName.$touch()"
-                                            @blur="$v.member.firstName.$touch()"
-                                            required
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col cols="12">
-                                        <v-text-field
-                                            dense
-                                            label="Date de naissance *"
-                                            v-model="member.birthDate"
-                                            type="text"
-                                            :error-messages="birthDateErrors"
-                                            @input="$v.member.birthDate.$touch()"
-                                            @blur="$v.member.birthDate.$touch()"
-                                            required
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col cols="12">
-                                        <v-select
-                                            v-model="member.gender"
-                                            :items="[{value: 'MALE', text: 'Homme'}, {value: 'FEMALE', text: 'Femme'}]"
-                                            label="Sexe *"
-                                            required
-                                            :error-messages="genderErrors"
-                                            @input="$v.member.gender.$touch()"
-                                            @blur="$v.member.gender.$touch()"
-                                        ></v-select>
-                                    </v-col>
-                                    <v-col cols="12">
-                                        <v-text-field
-                                            dense
-                                            label="E-mail *"
-                                            v-model="member.email"
-                                            type="text"
-                                            :error-messages="emailErrors"
-                                            @input="$v.member.email.$touch()"
-                                            @blur="$v.member.email.$touch()"
-                                            required
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col cols="12">
-                                        <v-text-field
-                                            dense
-                                            label="Mobile *"
-                                            v-model="member.mobile"
-                                            type="text"
-                                            :error-messages="mobileErrors"
-                                            @input="$v.member.mobile.$touch()"
-                                            @blur="$v.member.mobile.$touch()"
-                                            required
-                                        ></v-text-field>
-                                    </v-col>
-                                </v-row>
-                                <small>* champs obligatoires</small>
-                            </v-container>
-                        </v-card-text>
-                        <v-card-actions>
-                            <v-spacer></v-spacer>
-                            <v-btn color="blue darken-1" text @click="reinitMember">Réinitialiser</v-btn>
-                            <v-btn type="submit" dark color="blue darken-1">Enregistrer</v-btn>
-                        </v-card-actions>
-                    </v-form>
-                </v-card>
+                <member-edit-page-edition-form v-if="isMemberLoaded"
+                                               :initial-member="member"
+                                               :handle-submit="handleChangeContactInformation"></member-edit-page-edition-form>
             </v-col>
             <v-col md="8">
                 <v-tabs
@@ -185,19 +96,20 @@
 
 <script>
 import SimpleLayout from '@/pages/layouts/SimpleLayout'
-import { validationMixin } from 'vuelidate'
 import { memberService } from '@/services/member.service'
-import { email, required } from 'vuelidate/lib/validators'
+import MemberEditPageEditionForm from '@/pages/members/MemberEditPageEditionForm'
 
 export default {
     name: 'MemberEdit',
-    components: { SimpleLayout },
-    mixins: [validationMixin],
+    components: {
+        SimpleLayout,
+        MemberEditPageEditionForm
+    },
     data: function() {
         return {
             tab: null,
             memberId: this.$route.params['memberId'],
-            memberBackup: {},
+            isMemberLoaded: false,
             member: {
                 lastName: '',
                 firstName: '',
@@ -212,78 +124,13 @@ export default {
                     city: '',
                     zipCode: ''
                 }
-            },
-            loading: true
-        }
-    },
-    validations: {
-        member: {
-            lastName: {
-                required
-            },
-            firstName: {
-                required
-            },
-            gender: {
-                required
-            },
-            email: {
-                required,
-                email
-            },
-            mobile: {
-                required
-            },
-            birthDate: {
-                required
             }
         }
     },
-    computed: {
-        memberFullName() {
-            return `${this.member.firstName} ${this.member.lastName}`
-        },
-        emailErrors() {
-            const errors = []
-            if (!this.$v.member.email.$dirty) return errors
-            !this.$v.member.email.required && errors.push('L\'email est obligatoire')
-            !this.$v.member.email.email && errors.push('Format de l\'email incorrect')
-            return errors
-        },
-        firstNameErrors() {
-            const errors = []
-            if (!this.$v.member.firstName.$dirty) return errors
-            !this.$v.member.firstName.required && errors.push('Le prénom est obligatoire')
-            return errors
-        },
-        lastNameErrors() {
-            const errors = []
-            if (!this.$v.member.lastName.$dirty) return errors
-            !this.$v.member.lastName.required && errors.push('Le nom est obligatoire')
-            return errors
-        },
-        genderErrors() {
-            const errors = []
-            if (!this.$v.member.gender.$dirty) return errors
-            !this.$v.member.gender.required && errors.push('Le sexe est obligatoire')
-            return errors
-        },
-        mobileErrors() {
-            const errors = []
-            if (!this.$v.member.mobile.$dirty) return errors
-            !this.$v.member.mobile.required && errors.push('Le mobile est obligatoire')
-            return errors
-        },
-        birthDateErrors() {
-            const errors = []
-            if (!this.$v.member.birthDate.$dirty) return errors
-            !this.$v.member.birthDate.required && errors.push('La date de naissance est obligatoire')
-            return errors
-        }
-    },
     methods: {
-        reinitMember() {
-            this.member = JSON.parse(JSON.stringify(this.memberBackup))
+        handleChangeContactInformation() {
+            alert('submit')
+            console.table(this.member)
         },
         async handleChangeAddress() {
             await memberService.changeAddress(this.memberId, this.member.address)
@@ -291,7 +138,8 @@ export default {
     },
     async created() {
         this.member = await memberService.fetchMember(this.memberId)
-        let emptyAddress = {
+        this.isMemberLoaded = true
+        const emptyAddress = {
             line1: '',
             line2: '',
             line3: '',
@@ -299,8 +147,6 @@ export default {
             zipCode: ''
         }
         this.member.address = this.member.address || emptyAddress
-        this.memberBackup = JSON.parse(JSON.stringify(this.member))
-        this.loading = false
     }
 }
 </script>
