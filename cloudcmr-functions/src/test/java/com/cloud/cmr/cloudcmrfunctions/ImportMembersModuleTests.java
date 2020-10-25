516287acf0e25908bacb48475c79b17ee35d960f
truar <thibault.ruaro@gmail.com>
2020-10-25 20:17:48 +0100

    Add functions to cloud cmr

---
+++ cloudcmr-functions/src/test/java/com/cloud/cmr/cloudcmrfunctions/ImportMembersModuleTests.java
@@ -0,0 +1,13 @@
+package com.cloud.cmr.cloudcmrfunctions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportMembersModuleTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
