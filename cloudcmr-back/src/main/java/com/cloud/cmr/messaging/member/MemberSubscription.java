8136024e385351f96837fc322768db5ccfc5bc13
truar <thibault.ruaro@gmail.com>
2020-11-05 21:05:11 +0100

    Add subscription name as a var

--- cloudcmr-back/src/main/java/com/cloud/cmr/messaging/member/MemberSubscription.java
+++ cloudcmr-back/src/main/java/com/cloud/cmr/messaging/member/MemberSubscription.java
@@ -3,6 +3,7 @@ package com.cloud.cmr.messaging.member;
 import com.cloud.cmr.application.member.ImportMemberCommand;
 import com.cloud.cmr.application.member.MemberManager;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.client.util.Value;
 import com.google.pubsub.v1.PubsubMessage;
 import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
 import org.springframework.cloud.gcp.pubsub.support.converter.JacksonPubSubMessageConverter;
@@ -15,6 +16,8 @@ public class MemberSubscription {
 
     private final PubSubTemplate pubSubTemplate;
     private final MemberManager memberManager;
+    @Value("${subscription.name}")
+    private String subscriptionName;
 
     public MemberSubscription(PubSubTemplate pubSubTemplate, MemberManager memberManager) {
         this.pubSubTemplate = pubSubTemplate;
@@ -24,7 +27,7 @@ public class MemberSubscription {
 
     @PostConstruct
     public void configureSubscription() {
-        this.pubSubTemplate.subscribe("memberServiceSubscription", basicAcknowledgeablePubsubMessage -> {
+        this.pubSubTemplate.subscribe(subscriptionName, basicAcknowledgeablePubsubMessage -> {
             System.out.println("Receiving a message");
             PubsubMessage pubsubMessage = basicAcknowledgeablePubsubMessage.getPubsubMessage();
             MemberExternalDataDTO memberExternalDataDTO = this.pubSubTemplate.getMessageConverter().fromPubSubMessage(pubsubMessage, MemberExternalDataDTO.class);
@@ -34,7 +37,7 @@ public class MemberSubscription {
                     memberExternalDataDTO.getFirstName(),
                     memberExternalDataDTO.getEmail(),
                     memberExternalDataDTO.getBirthDate(),
-                    memberExternalDataDTO.getGender(),
+                    memberExternalDataDTO.getGender().equals("M") ? "MALE" : "FEMALE",
                     memberExternalDataDTO.getPhone(),
                     memberExternalDataDTO.getMobile(),
                     memberExternalDataDTO.getLine1(),
